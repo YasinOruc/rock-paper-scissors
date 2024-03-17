43b5a708e8eaf340ec19b33838156e5fd81c3723
YasinOruc <orucyasin14@gmail.com>
2024-03-17 11:19:00 +0100

    finished playRound function

--- main.js
+++ main.js
@@ -4,3 +4,25 @@ function getComputerChoice(){
     return choice
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose, scissors beats paper!";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win, scissors beats paper!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win, rock beats scissors!";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose, rock beats scissors!";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win, paper beats rock!";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose, paper beats rock!";
+    } else if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else {
+        return "Invalid input";
+    }
+}
